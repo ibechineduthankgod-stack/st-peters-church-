fe1f3907d10e06c5209386ed253d29e98f014c85
ibechineduthankgod-stack <ibechineduthankgod@gmail.com>
2026-04-08 13:20:11 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -44,3 +44,9 @@ popup.style.display="none";
 };
 
 }
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+navLinks.classList.toggle("active");
+});
